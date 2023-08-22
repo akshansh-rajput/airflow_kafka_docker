f512f83015a5ee2a0819cd735b75e407435c6d89
Akshansh Rajput <ext_akraj@arlafoods.com>
2023-08-22 20:38:06 +0300

    added kafka py

---
+++ kafka/admin/delete.py
@@ -0,0 +1,17 @@
+from confluent_kafka.admin import AdminClient
+
+a = AdminClient({'bootstrap.servers': '127.0.0.1:9092'})
+
+topic_to_delete = ["test_topic"]
+
+# Call create_topics to asynchronously create topics. A dict
+# of <topic,future> is returned.
+result = a.delete_topics(topic_to_delete)
+
+# Wait for each operation to finish.
+for topic, f in result.items():
+    try:
+        f.result()  # The result itself is None
+        print("Topic {} deleted".format(topic))
+    except Exception as e:
+        print("Failed to delete topic {}: {}".format(topic, e))
